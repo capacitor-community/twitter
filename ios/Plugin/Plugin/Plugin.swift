201cadf6ee2adb823cafb68220fda10bc0f7ac73
Stewan <oi@stewan.io>
2019-03-22 20:33:56 -0300

    fix(ios): remove extra setup from AppDelegate

--- ios/Plugin/Plugin/Plugin.swift
+++ ios/Plugin/Plugin/Plugin.swift
@@ -18,7 +18,21 @@ public class TwitterPlugin: CAPPlugin
         let consumerSecret = getConfigValue("consumerSecret") as? String ?? "ADD_IN_CAPACITOR_CONFIG_JSON"
         
         TWTRTwitter.sharedInstance().start(withConsumerKey: consumerKey, consumerSecret: consumerSecret)
+        
+        NotificationCenter.default.addObserver(self, selector: #selector(self.didTwitterRespond(notification:)), name: Notification.Name(CAPNotifications.URLOpen.name()), object: nil)
     }
+    
+ 
+    @objc func didTwitterRespond(notification: NSNotification) {
+        let app = UIApplication.shared
+        
+        guard let object = notification.object as? [String:Any?] else {
+            return
+        }
+
+        TWTRTwitter.sharedInstance().application(app, open: object["url"] as! URL, options: object["options"] as! [AnyHashable : Any])
+    }
+    
     
     @objc func isLogged(_ call: CAPPluginCall) {
         DispatchQueue.main.async {
